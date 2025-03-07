3bfb2d9331c222379b0f52fa357696a77d0a2509
Sergey Kacheev <S.Kacheev@gmail.com>
2025-03-07 11:37:08 +0300

    feat: add FuzzMapTrieInsertAndLookup for MapTrie (#49)
    * feat: add FuzzMapTrieInsertAndLookup for MapTrie
    
    * chore: add TestMapTrieLookupv6

--- controlplane/modules/route/internal/rib/map_trie_test.go
+++ controlplane/modules/route/internal/rib/map_trie_test.go
@@ -37,6 +37,35 @@ func TestMapTrieInsert(t *testing.T) {
 	}
 }
 
+func TestMapTrieLookupv6(t *testing.T) {
+	addr := netip.MustParseAddr("fd25:cf19:6b13:cafe:babe:be57:f00d:0001")
+	cases := []struct {
+		prefix string
+		match  bool
+	}{
+		{"fd25:8888:6b13:cafe:babe:be57:f00d:04a5/16", true},
+		{"fd25:8888:6b13:cafe:babe:be57:f00d:04a5/64", false},
+		{"fd25:cf19:6b13:cafe:babe:be57:f00d:04a5/64", true},
+		{"fd25:cf19:6b13:cafe:babe:be57:f00d:04a5/112", true},
+		{"fd25:cf19:6b13:cafe:babe:be57:f00d:04a5/120", false},
+		{"fd25:cf19:6b13:cafe:babe:be57:f00d:04a5/128", false},
+	}
+	mt := NewMapTrie(0)
+	for _, c := range cases {
+		prefix := netip.MustParsePrefix(c.prefix).Masked()
+
+		route := Route{
+			MapTrieKey: MapTrieKey{Prefix: prefix},
+		}
+		mt.InsertOrUpdate(route)
+		list, ok := mt.Lookup(addr)
+		mp := list.Routes[0].Prefix
+		ok = ok && prefix == mp
+		require.Equal(t, c.match, ok, "lookup expected to match==%t but ok=%t addr=%s prefix=%s mp=%s",
+			c.match, ok, addr, prefix, mp)
+	}
+}
+
 func TestMapTrieMatches(t *testing.T) {
 	cases := []struct {
 		prefix  string
@@ -48,6 +77,10 @@ func TestMapTrieMatches(t *testing.T) {
 		{"192.168.10.1/24", false},
 		{"192.168.9.1/24", true},
 		{"192.168.9.1/16", true},
+		{"a8c0:109::/16", false},       // 192.168.9.1 in hex
+		{"a8c0:109::/112", false},      // 192.168.9.1 in hex
+		{"::ffff:a8c0:109/16", false},  // v6 mapped ::ffff:168.192.1.9
+		{"::ffff:a8c0:109/112", false}, // v6 mapped ::ffff:168.192.1.9
 		{"192.168.18.0/8", true},
 		{"193.168.9.1/8", false},
 		{"192.168.18.0/0", true},
@@ -68,6 +101,55 @@ func TestMapTrieMatches(t *testing.T) {
 		require.Equal(t, expected, actual)
 	}
 
+}
+
+func FuzzMapTrieInsertAndLookup(f *testing.F) {
+	addr := netip.MustParseAddr("fd25:c819:6888:0:b282:ffff:1841:3832").As16()
+	allZero := netip.IPv6Unspecified().As16()
+	allFF := netip.MustParseAddr("ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff").As16()
+
+	f.Add(byte(120), allZero[:], addr[:])
+	f.Add(byte(30), addr[:], allFF[:])
+	f.Add(byte(0), addr[:], addr[:])
+	f.Add(byte(100), allZero[:], allFF[:])
+	f.Add(byte(128), allFF[:], allFF[:])
+
+	f.Fuzz(func(t *testing.T, m byte, pb []byte, qb []byte) {
+		mt := NewMapTrie(0)
+
+		prefixBytes := [16]byte{}
+		copy(prefixBytes[:], pb)
+		prefixAddr := netip.AddrFrom16(prefixBytes)
+
+		m = min(m, 128)
+		p := netip.PrefixFrom(prefixAddr, int(m)).Masked()
+
+		route := Route{MapTrieKey: MapTrieKey{Prefix: p}}
+		mt.InsertOrUpdate(route)
+
+		queryBytes := [16]byte{}
+		copy(queryBytes[:], qb)
+		queryAddr := netip.AddrFrom16(queryBytes)
+
+		_, ok := mt.Lookup(queryAddr)
+		qaPrefix := netip.PrefixFrom(queryAddr, int(m)).Masked()
+		equal := p == qaPrefix
+
+		switch [2]bool{ok, equal} {
+		case [2]bool{false, true}:
+			t.Errorf("query addr %s should match %s", queryAddr, p)
+		case [2]bool{true, false}:
+			t.Errorf("unexpected match of addr %s by prefix %s", qaPrefix, p)
+		}
+		matches := mt.Matches(queryAddr)
+		matched := len(matches) > 0
+		switch [2]bool{ok && equal, matched} {
+		case [2]bool{false, true}:
+			t.Errorf("unexpected return from Matches: %s, prefix=%s, queryAddr=%s", matches, p, queryAddr)
+		case [2]bool{true, false}:
+			t.Errorf("Matches should return a match: prefix=%s, queryAddr=%s", p, queryAddr)
+		}
+	})
 }
 
 func heapInUse() uint64 {
