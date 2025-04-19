b647957c29ec5366622f3c88e2b99ae7c6de04c9
Sergey Kacheev <S.Kacheev@gmail.com>
2025-04-19 15:48:52 +0300

    feat: add fuzzing for forward module (#137)
    * feat: add fuzzing for forward module
    
    * fix: pass invalid packets to the packet handler

--- modules/forward/fuzzing/forward.c
+++ modules/forward/fuzzing/forward.c
@@ -5,15 +5,15 @@
 #include "dataplane/module/module.h"
 #include "dataplane/module/testing.h"
 #include "dataplane/packet/packet.h"
-#include "modules/decap/api/controlplane.h"
-#include "modules/decap/dataplane/config.h"
-#include "modules/decap/dataplane/dataplane.h"
+#include "modules/forward/api/controlplane.h"
+#include "modules/forward/dataplane/config.h"
+#include "modules/forward/dataplane/dataplane.h"
 
 #define ARENA_SIZE (1 << 20)
 #define MBUF_MAX_SIZE 8196
 #define RTE_PKTMBUF_HEADROOM 256
 
-struct decap_fuzzing_params {
+struct forward_fuzzing_params {
 	struct module *module; /**< Pointer to the module being tested */
 	struct module_data *module_data; /**< Module configuration */
 
@@ -23,15 +23,19 @@ struct decap_fuzzing_params {
 	struct memory_context mctx;
 };
 
-static struct decap_fuzzing_params fuzz_params = {
+static struct forward_fuzzing_params fuzz_params = {
 	.module_data = NULL,
 };
 
 static int
-decap_test_config(struct module_data **module_data) {
-	struct decap_module_config *config =
-		(struct decap_module_config *)memory_balloc(
-			&fuzz_params.mctx, sizeof(struct decap_module_config)
+forward_test_config(struct module_data **module_data) {
+	uint16_t device_count = 2;
+
+	struct forward_module_config *config =
+		(struct forward_module_config *)memory_balloc(
+			&fuzz_params.mctx,
+			sizeof(struct forward_module_config
+			) + sizeof(struct forward_device_config) * device_count
 		);
 
 	if (!config) {
@@ -40,52 +44,77 @@ decap_test_config(struct module_data **module_data) {
 
 	// Initialize module_data fields
 	strtcpy(config->module_data.name,
-		"decap_test",
+		"forward_test",
 		sizeof(config->module_data.name));
 	memory_context_init_from(
 		&config->module_data.memory_context,
 		&fuzz_params.mctx,
-		"decap_test"
+		"forward_test"
 	);
 
 	config->module_data.index = 0;
 	config->module_data.agent = NULL;
-	config->module_data.free_handler = decap_module_config_free;
+	config->device_count = device_count;
+	config->module_data.free_handler = forward_module_config_free;
 
 	struct memory_context *memory_context =
 		&config->module_data.memory_context;
-	if (lpm_init(&config->prefixes4, memory_context)) {
-		goto error_lpm_v4;
-	}
-	if (lpm_init(&config->prefixes6, memory_context)) {
-		goto error_lpm_v6;
-	}
 
-	// 127.0.0.0/24
-	decap_module_config_add_prefix_v4(
-		&config->module_data,
-		(uint8_t[4]){127, 0, 0, 0},
-		(uint8_t[4]){127, 0, 0, 0xff}
-	);
-	// fe80::0/96
-	decap_module_config_add_prefix_v6(
-		&config->module_data,
-		(uint8_t[16]){0xfe, 0x80, [15] = 0},
-		(uint8_t[16]
-		){0xfe, 0x80, [12] = 0xff, [13] = 0xff, [14] = 0xff, [15] = 0xff
+	for (uint16_t dev_idx = 0; dev_idx < device_count; ++dev_idx) {
+		struct forward_device_config *device_forward =
+			config->device_forwards + dev_idx;
+		device_forward->l2_forward_device_id = dev_idx;
+		if (lpm_init(&device_forward->lpm_v4, memory_context)) {
+			goto fail;
 		}
-	);
+		if (lpm_init(&device_forward->lpm_v6, memory_context)) {
+			goto fail;
+		}
+	}
+	for (uint16_t idx = 0; idx < device_count; idx++) {
+		uint16_t from_dev = idx;
+		uint16_t to_dev = device_count - idx - 1;
+		int rc = forward_module_config_enable_l2(
+			&config->module_data, from_dev, to_dev
+		);
+		if (rc != 0) {
+			goto fail;
+		}
+
+		// 127.0.0.0/24
+		rc = forward_module_config_enable_v4(
+			&config->module_data,
+			(uint8_t[4]){127, 0, 0, 0},
+			(uint8_t[4]){127, 0, 0, 0xff},
+			from_dev,
+			to_dev
+		);
+		if (rc != 0) {
+			goto fail;
+		}
+		// fe80::0/96
+		rc = forward_module_config_enable_v6(
+			&config->module_data,
+			(uint8_t[16]){0xfe, 0x80, [15] = 0},
+			(uint8_t[16]){0xfe,
+				      0x80,
+				      [12] = 0xff,
+				      [13] = 0xff,
+				      [14] = 0xff,
+				      [15] = 0xff},
+			from_dev,
+			to_dev
+		);
+		if (rc != 0) {
+			goto fail;
+		}
+	}
 
 	*module_data = (struct module_data *)config;
 	return 0;
 
-error_lpm_v6:
-	lpm_free(&config->prefixes4);
-
-error_lpm_v4:
-	memory_bfree(
-		&fuzz_params.mctx, config, sizeof(struct decap_module_config)
-	);
+fail:
+	forward_module_config_free(&config->module_data);
 	return -EINVAL;
 }
 
@@ -102,10 +131,10 @@ fuzz_setup() {
 	);
 
 	memory_context_init(
-		&fuzz_params.mctx, "decap fuzzing", &fuzz_params.ba
+		&fuzz_params.mctx, "forward fuzzing", &fuzz_params.ba
 	);
 
-	fuzz_params.module = new_module_decap();
+	fuzz_params.module = new_module_forward();
 	fuzz_params.payload_arena = memory_balloc(
 		&fuzz_params.mctx,
 		sizeof(struct packet_front) + MBUF_MAX_SIZE * 4
@@ -114,7 +143,7 @@ fuzz_setup() {
 		return -ENOMEM;
 	}
 
-	return decap_test_config(&fuzz_params.module_data);
+	return forward_test_config(&fuzz_params.module_data);
 }
 
 int
@@ -139,7 +168,7 @@ LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) { // NOLINT
 	);
 
 	parse_packet(pf->input.first);
-	// Process packet through decap module
+	// Process packet through forward module
 	fuzz_params.module->handler(NULL, fuzz_params.module_data, pf);
 
 	return 0;
